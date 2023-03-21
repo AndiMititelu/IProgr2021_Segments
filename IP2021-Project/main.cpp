2f3b5b921e06a5c45752b62d194c01bca423129b
AndiMititelu <41987168+AndiMititelu@users.noreply.github.com>
2023-03-21 17:00:08 +0200

    Add files via upload

---
+++ IP2021-Project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "structuri.h"
+#include "generare_tabel.h"
+#include "menu.h"
+#include <winbgim.h>
+using namespace std;
+
+int main()
+{
+    Start_Game();
+    return 0;
+}
